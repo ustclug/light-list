b05efd3e418aee79f21b493c2fa235c586ee9245
Jiyan He <volltin@live.com>
2018-12-01 16:50:05 +0800

    rename after upload (avoid incomplete pac file)

--- tools/upload-pac.sh
+++ tools/upload-pac.sh
@@ -4,8 +4,10 @@ ftp -p -n $FTP_HOST << EOF
 quote USER $FTP_USER
 quote PASS $FTP_PASS
 cd light
-put http.pac
-put https.pac
+put http-temp.pac
+rename http-temp.pac http.pac
+put https-temp.pac
+rename https-temp.pac https.pac
 quit
 EOF
 
